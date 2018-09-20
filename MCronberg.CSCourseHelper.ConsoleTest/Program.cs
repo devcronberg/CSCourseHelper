1e353540f30c45eaffbd33897af2a603a06c4967
Michell Cronberg <admin@TA-828-INS>
2018-09-20 17:53:06 +0200

    Initial

---
+++ MCronberg.CSCourseHelper.ConsoleTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCronberg.CSCourseHelper.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            string t = "***";
+            
+            //int u = 1;
+            Console.WriteLine(t.ToStringEx());
+        }
+    }
+
+    class Person {
+        public int Id { get; set; }
+        public string Navn { get; set; }
+    }
+}
--- MCronberg.CSCourseHelper.ConsoleTest/Properties/AssemblyInfo.cs
+++ MCronberg.CSCourseHelper.ConsoleTest/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MCronberg.CSCourseHelper")]
+[assembly: AssemblyTitle("MCronberg.CSCourseHelper.ConsoleTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MCronberg.CSCourseHelper")]
-[assembly: AssemblyCopyright("Copyright © Michell Cronberg 2018")]
+[assembly: AssemblyProduct("MCronberg.CSCourseHelper.ConsoleTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ad1b3dc9-a753-4006-aea0-8465cff14e30")]
+[assembly: Guid("841953d8-293d-431f-b4ea-5d2627bee5c1")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
